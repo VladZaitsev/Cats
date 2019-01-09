df9991c2b9ebf991f66500f46b654b1d903dad1b
Vlad <32698146+VladZaytcev@users.noreply.github.com>
2019-01-09 21:11:27 +0800

    Create LowJumpBehavior.java

---
+++ LowJumpBehavior.java
@@ -0,0 +1,7 @@
+package object_oriented_programming;
+
+pubic class LowJumpBehavior() implements JumpBehavior{
+  public void jump(){
+      System.out.println("How does this cat jump? - Jumps low");
+  }
+}
