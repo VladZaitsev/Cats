56ea3f214001b66ac425f0fa64222fd796f0fe9b
Vlad <32698146+VladZaytcev@users.noreply.github.com>
2019-01-09 21:31:16 +0800

    Create HighJumpBehavior.java

---
+++ HighJumpBehavior.java
@@ -0,0 +1,7 @@
+package object_oriented_programming;
+
+pubic class HighJumpBehavior() implements JumpBehavior{
+  public void jump(){
+       System.out.println("How does this cat jump? - Jumps high");
+  }
+}
