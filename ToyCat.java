1e33b30396d00acf01034d44363e30eb462e52ae
Vlad <32698146+VladZaytcev@users.noreply.github.com>
2019-01-09 21:21:36 +0800

    Update ToyCat.java

--- ToyCat.java
+++ ToyCat.java
@@ -7,23 +7,15 @@ package object_oriented_programming;
  */
 public class ToyCat extends Cat {
 
+    public void ToyCat(){
+        purrBehavior = new NoPurrBehavior();
+        jumpBehavior = new NoJumpBehavior();
+        meowBehavior = new GeneralMeowBehavior();
+    }
+    
     public void display() {
         //display implementation
 
         System.out.println("Toy cat is displayed, it is soft");
-    }
-
-    @Override
-    public void purr() {
-        //do nothing
-
-        System.out.println("This cat does not purr!");
-    }
-
-    @Override
-    public void jump() {
-        //do nothing
-
-        System.out.println("This cat does not jump!");
     }
 }
