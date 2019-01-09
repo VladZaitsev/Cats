4a5b14decabafc9035bdb9e1fcee91f5afef06e7
Vlad <32698146+VladZaytcev@users.noreply.github.com>
2019-01-09 21:27:51 +0800

    Create GeneralPurrBehavior.java

---
+++ GeneralPurrBehavior.java
@@ -0,0 +1,7 @@
+package object_oriented_programming;
+
+pubic class GeneralPurrBehavior() implements PurrBehavior(){
+  public void jump(){
+     System.out.println("This cat can purr, mrrrrr...");
+  }
+}
