644f1c0f5e106ab3125ef6a2520630b8c016da8e
Vlad <32698146+VladZaytcev@users.noreply.github.com>
2019-01-09 21:18:22 +0800

    Create GeneralMeowBehavior.java

---
+++ GeneralMeowBehavior.java
@@ -0,0 +1,7 @@
+package object_oriented_programming;
+
+pubic class GeneralMeowBehavior() implements  MeowBehavior{
+  public void jump(){
+     System.out.println("Does this cat meow? - Meow!");
+  }
+}
