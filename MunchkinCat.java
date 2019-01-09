82f11e33283cdfb50b514aa1bfa3dffe9339e748
Vlad <32698146+VladZaytcev@users.noreply.github.com>
2019-01-09 21:32:36 +0800

    Update MunchkinCat.java

--- MunchkinCat.java
+++ MunchkinCat.java
@@ -7,17 +7,17 @@ ackage object_oriented_programming;
  */
 public class MunchkinCat extends Cat {
 
+    public MunchkinCat(){
+        purrBehavior = new GeneralPurrBehavior();
+        jumpBehavior = new LowJumpBehavior();
+        meowBehavior = new GeneralMeowBehavior();
+    }
+    
+    
     @Override
     public void display() {
         //display implementation
 
         System.out.println("Munchkin cat is displayed, he has small paws");
-    }
-
-    @Override
-    public void jump() {
-        //jump implementation
-
-        System.out.println("How does this cat jump? - Jumps low");
     }
 }
