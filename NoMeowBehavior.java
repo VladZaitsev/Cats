fc6d3c59084ddc17fb372d17d7c43f4cfd457cc5
Vlad <32698146+VladZaytcev@users.noreply.github.com>
2019-01-09 21:41:52 +0800

    Update NoMeowBehavior.java

--- NoMeowBehavior.java
+++ NoMeowBehavior.java
@@ -1,7 +1,7 @@
 package object_oriented_programming;
 
 pubic class NoMeowBehavior() implements  MeowBehavior{
-  public void jump(){
+  public void meow(){
      System.out.println("This cat does not mew!");
   }
 }
